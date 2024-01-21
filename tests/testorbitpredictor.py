96b52a61a6a9fc2fec0fb8f290e84d5b76ae5491
fcrt <you@example.com>
2024-01-21 18:17:11 +0000

    add testcode

--- tests/testorbitpredictor.py
+++ tests/testorbitpredictor.py
@@ -1,3 +1,4 @@
+import datetime
 import sys
 sys.path.append("../modules/orbit-predictor/")
 from orbit_predictor.sources import EtcTLESource
@@ -6,13 +7,28 @@ from orbit_predictor.locations import Location
 OXFORD = Location(
     "OXFORD", latitude_deg=-1.20187, longitude_deg=51.75524, elevation_m=26.00)
 
+print(OXFORD)
+
 source = EtcTLESource(filename="../trajectorydata/240111iss.tle")
 predictor = source.get_predictor("ISS")
 
-predicted_pass = predictor.get_next_pass(OXFORD)
+predicted_next_pass = predictor.get_next_pass(OXFORD)
+print(predicted_next_pass)
 
 
-print(predicted_pass)
+utc_nextpass = predicted_next_pass.aos
+print(utc_nextpass)
+#predicted_nextnext_pass = predictor.get_next_pass(OXFORD, when_utc=utc_nextpass+ datetime.timedelta(minutes=20))
+#print(predicted_nextnext_pass)
+predicted_following_pass = predicted_next_pass
+for i in range (1,15):
+   predicted_following_pass = predictor.get_next_pass(OXFORD, when_utc = predicted_following_pass.aos + datetime.timedelta(minutes = 20 ))
+   position_ecef = predictor.get_position(predicted_following_pass.aos)
+   position_llh = position_ecef.position_llh
+   print(predicted_following_pass, position_llh)
+
+"""
+
 
 position = predictor.get_position(predicted_pass.aos)
 
@@ -34,3 +50,5 @@ tomorrow = datetime.datetime.utcnow() + datetime.timedelta(days=1)
 
 next_predicted_path_tomorrow = predictor.get_next_pass(OXFORD, tomorrow, max_elevation_gt=20)
 print(next_predicted_path_tomorrow)
+
+"""
