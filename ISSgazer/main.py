1b13f522c7a06714147e6fc24a5deb47fa244eab
fcrt <you@example.com>
2024-02-04 16:52:14 +0000

    added city selection

--- ISSgazer/main.py
+++ ISSgazer/main.py
@@ -1,13 +1,37 @@
 import PySimpleGUI as sg
 import datetime as dt
+import json
 
 def printDeltaTime(deltaSeconds = 0):
    t = (dt.datetime.utcnow() + dt.timedelta(seconds = deltaSeconds ))     
    return t.strftime('%a %d %b %Y, %I:%M%p')
 
+def getCoordinates(country, city):
+  cities = [c for c in citiesCoordinateDict if c['country']==country]
+  selectedCity = [c for c in cities if c['name']==city]
+  print(selectedCity[0])
+  return selectedCity[0]['lat'], selectedCity[0]['lng']
 
+
+with open('../data/cities.json') as cities_file:
+  cities_file_content = cities_file.read()
+citiesCoordinateDict = json.loads(cities_file_content)
+countriesList = list(set([c['country'] for c in citiesCoordinateDict]))
+countriesList.sort()
+
+selectedCountry = 'GB'
+selectedCity = 'Oxford'
+
+lat, lng = getCoordinates('GB','Oxford')
+
+
+citiesList=[c['name'] for c in citiesCoordinateDict if c['country']==selectedCountry]
+citiesList.sort()
 layout = [
     [sg.Text("ISS Gazer")],
+    [sg.Combo(countriesList, default_value=selectedCountry, s=(15,22), enable_events=True, readonly=True, k='-LOCATION_COUNTRY-')],
+    [sg.Combo(citiesList, default_value=selectedCity, s=(15,22), enable_events=True, readonly=True, k='-LOCATION_CITY-')],
+    [sg.Text("Coordinates:  " + str(lat )+"  , "+str( lng) , key="coordinatesText" )],
     [sg.Text(""+ printDeltaTime(), key="datetimeText" )], 
     [sg.Button("Reset")],
     [sg.Button("Play")],
@@ -41,4 +65,12 @@ while True:
         window['datetimeText'].update( printDeltaTime(deltaSeconds=issDeltaTime) )   
     if event == "Play":
          timeout = 100
+    if event == '-LOCATION_COUNTRY-':
+         selectedCountry = values['-LOCATION_COUNTRY-']
+         citiesList=[c['name'] for c in citiesCoordinateDict if c['country']==selectedCountry]
+         citiesList.sort()
+         window['-LOCATION_CITY-'].update(values=citiesList)
+    if event == '-LOCATION_CITY-':
+         lat, lng = getCoordinates(values['-LOCATION_COUNTRY-'], values['-LOCATION_CITY-'])
+         window['coordinatesText'].update("Coordinates:  " + str(lat)+"  , "+str(lng))
 print('Shutting down...')       
